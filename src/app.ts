7baad3de3dadcfe4bda17b8b5f957b5dd417c076
Yeongil Yoon <askme143@kaist.ac.kr>
2020-11-21 21:12:19 +0900

    Add uptime request

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,7 @@ const client = new Client();
 
 client.on('ready', () => {
   if (client.user) {
-    client.user.setAvatar('./public/profile.PNG');
+    client.user.setAvatar('./public/profile.PNG').catch();
   }
 });
 
@@ -21,6 +21,18 @@ client.on('message', (msg) => {
   /* Routing */
   if (request === 'ping' && client.ws.ping) {
     msg.channel.send(`Pong! ${Math.floor(client.ws.ping)}ms`);
+  }
+  if (request === 'uptime' && client.uptime) {
+    let t = client.uptime;
+
+    const ms = t % 1000;
+    t = (t - ms) / 1000;
+    const secs = t % 60;
+    t = (t - secs) / 60;
+    const mins = t % 60;
+    const hrs = (t - mins) / 60;
+
+    msg.channel.send(`${hrs}시간 ${mins}분  ${secs}.${ms}초`);
   }
   if (request === 'q') {
     const arg = msg.content.substr(3).trim();
